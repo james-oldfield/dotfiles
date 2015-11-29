1932cf1c975cea0f6e15f3703ea90c1d90789cce
James Oldfield <hi@james-oldfield.co>
2015-11-29 14:24:23 +0000

    hush login and small updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "brew.sh" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "npm.sh" --exclude "brewcask.sh" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
